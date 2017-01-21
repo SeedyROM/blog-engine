c8e0d6d706447312f84f7776ba1ed10b63b3a39b
Zack Kollar <rallokkcaz@gmail.com>
2017-01-21 18:36:37 -0500

    Setup static file serving, fixed bugs and finished default page.

--- app.js
+++ app.js
@@ -6,20 +6,19 @@ const isDev = app.get('env') === 'development'
 const expressNunjucks = require('express-nunjucks')
 const path = require('path')
 const port = 3000
+const serveStaticHTML = false
+const staticHTMLLocation = 'public/html'
+const useTemplates = true
+const templatesLocation = '/templates'
 
-// Displays server log in the CLI
 app.use(logger('dev'))
+app.use('/js', express.static('public/js'))
+app.use('/stylesheets', express.static('public/stylesheets'))
+if(serveStaticHTML) app.use(express.static(staticHTMLLocation))
+if(useTemplates) app.set('views', __dirname + templatesLocation)
+const njk = expressNunjucks(app, { watch: isDev, noCache: isDev })
 
-// instruct express to server up static assets
-app.use(express.static('public'))
-app.set('views', __dirname + '/templates')
-
-const njk = expressNunjucks(app, {
-    watch: isDev,
-    noCache: isDev
-})
-
-app.get('/home', (req, res) => {
+app.get('/', (req, res) => {
   res.render('index', {
     appName: (() => {
       var fs = require('fs')
--- public/js/application.js
+++ public/js/application.js
@@ -2,5 +2,14 @@ console.clear();
 
 $(document).ready(function() {
 	$('.title')
-		.velocity({ opacity: [1, 'ease'], fontSize: [parseInt($('.title').css('font-size')) + 10, 'spring']}, { duration: 2000 })
+		.velocity(
+			{
+				opacity: [1, 'ease'],
+				fontSize: [parseInt($('.title').css('font-size')) + 10, 'spring']
+			},
+			{
+				delay: 500,
+				duration: 2000
+			}
+		)
 })

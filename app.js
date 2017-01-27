b381f4e41583d7788efe79704e58b6ae01975d72
Zack Kollar <rallokkcaz@gmail.com>
2017-01-26 21:04:51 -0500

    Added more command line support and colorized output.

--- app.js
+++ app.js
@@ -1,23 +1,53 @@
 // set variables for environment
 const express = require('express')
 const logger = require('morgan')
+const colors = require('colors')
+colors.setTheme({
+  silly: 'rainbow', input: 'grey', verbose: 'cyan',
+  prompt: 'grey', info: 'green', data: 'grey',
+  help: 'cyan', warn: 'yellow', debug: 'blue', error: 'red'
+})
 const app = express()
-const isDev = app.get('env') === 'development'
-const expressNunjucks = require('express-nunjucks')
+
+const exitHandler = (code) => {
+  if(code != 0) console.log(`Server shutting down with code: ${code}`.warn);
+  else console.log(`Server shutting down with code: ${code}`.info);
+}
+process.on('exit', exitHandler);
+process.on('SIGINT', () => { console.log(''); process.exit(0) })
+
 const path = require('path')
+const hostname = 'localhost'
 const port = 3000
+
 const serveStaticHTML = false
 const staticHTMLLocation = 'public/html'
 const useTemplates = true
 const templatesLocation = '/templates'
+const isDev = app.get('env') === 'development'
+const expressNunjucks = require('express-nunjucks')
 
+// setup mongodb connection
+const mongoose = require('mongoose')
+mongoose.connect(`mongodb://${hostname}/test`)
+mongoose.connection.on('error', (error) => {
+  console.log(colors.error(error))
+  console.log('Error connecting to MongoDB!'.error)
+  process.exit(1);
+})
+mongoose.connection.once('open', () => {
+  console.log('Connected to mongodb...'.input)
+})
+
+// basic config
 app.use(logger('dev'))
 app.use('/js', express.static('public/js'))
 app.use('/stylesheets', express.static('public/stylesheets'))
 if(serveStaticHTML) app.use(express.static(staticHTMLLocation))
 if(useTemplates) app.set('views', __dirname + templatesLocation)
-const njk = expressNunjucks(app, { watch: isDev, noCache: isDev })
+expressNunjucks(app, { watch: isDev, noCache: isDev })
 
+// default route
 app.get('/', (req, res) => {
   res.render('index', {
     appName: (() => {
@@ -28,7 +58,7 @@ app.get('/', (req, res) => {
   });
 })
 
-// Set server port
-app.listen(port)
-
-console.log("Server is running at => http://localhost:" + port + "/\nCTRL + C to shutdown")
+// start the server
+app.listen(port, () => {
+  console.log(`Server started at ${hostname}:${port}...`.info)
+})

15ca28ea9b2fc3ee2dc00621c7f39a18365ef156
Zack Kollar <rallokkcaz@gmail.com>
2017-01-31 20:02:21 -0500

    Starting work on the database models/relationships for the blog.

--- app.js
+++ app.js
@@ -2,6 +2,8 @@
 require('./config/sys')
 require('./config/mongoose')
 require('./config/passport')
+const Post = require('./lib/models/post.js')
+const Tag = require('./lib/models/tag.js')
 
 // Load routes
 require('./lib/router')
